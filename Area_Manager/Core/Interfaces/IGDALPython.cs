c1bb0f8259aeef33a2c5414cbb1edb1c1fbf4989
Naillin <kamilasd@yandex.ru>
2026-04-11 17:01:50 +0500

    to last commit

---
+++ Area_Manager/Core/Interfaces/IGDALPython.cs
@@ -0,0 +1,10 @@
+﻿namespace Area_Manager.Core.Interfaces;
+
+public interface IGDALPython
+{
+    public void StartPythonProcess();
+    
+    public bool HealthCheck();
+    
+    public Task<double> GetElevation(Coordinate coordinate, CancellationToken cancellationToken = default);
+}
