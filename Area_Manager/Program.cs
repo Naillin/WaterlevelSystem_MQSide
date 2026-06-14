74dc4912e93e654c371ca26f4e588a2de132bf54
Naillin <kamilasd@yandex.ru>
2026-06-14 15:48:34 +0500

    minor change

--- Area_Manager/Program.cs
+++ Area_Manager/Program.cs
@@ -94,7 +94,7 @@ internal class Program
 			services.AddHostedService<MQConnectorWorker>();
 
 			services.AddSingleton<ITrendCalculator, LinearTrendCalculator>();
-			services.AddSingleton<IMovingAverage>(provider => new ExponentialMovingAverage(0.50)); // todo: убрать хардкод
+			services.AddSingleton<IMovingAverage>(provider => new ExponentialMovingAverage(0.35)); // todo: убрать хардкод
 			services.AddSingleton<IPredictor, Predictor>(provider =>
 			{
 				var trendCalculator = provider.GetRequiredService<ITrendCalculator>();
